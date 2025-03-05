03fce6ee8b0a29fdc0eac06f181c2b61ec8e7b3c
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-03-05 14:12:36 +0000

    add more tests

--- tst/rename_applier/test_simple_rename_applier.py
+++ tst/rename_applier/test_simple_rename_applier.py
@@ -1,12 +1,13 @@
 import os
+from parameterized import parameterized
 from unittest import TestCase
 from typing import override
 from src.professional_file_renamer.rename_applier.simple_rename_applier import SimpleRenameApplier
 
 
 class TestSimpleRenameApplier(TestCase):
-    TEST_DIRECTORY: str = "test_dir"
-    TEST_FILE: str = "test_file.txt"
+    TEST_DIR_NAME: str = "test_dir"
+    TEST_FILE_NAME: str = "test_file.txt"
 
     subject: SimpleRenameApplier
 
@@ -14,16 +15,43 @@ class TestSimpleRenameApplier(TestCase):
     def setUp(self):
         self.subject: SimpleRenameApplier = SimpleRenameApplier()
         # create sample test directory and file
-        os.mkdir(self.TEST_DIRECTORY)
-        open(os.path.join(self.TEST_DIRECTORY, self.TEST_FILE), "w").close()
+        os.mkdir(self.TEST_DIR_NAME)
+        open(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME), "w").close()
 
     @override
     def tearDown(self):
         # delete sample test directory and file
-        os.remove(os.path.join(self.TEST_DIRECTORY, self.TEST_FILE))
-        os.rmdir(self.TEST_DIRECTORY)
+        os.remove(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME))
+        os.rmdir(self.TEST_DIR_NAME)
 
-    def test_rename(self):
-        self.fail()
+    @parameterized.expand([
+        ("test_case_1", "non_existent_directory", "file1.txt", "file1.txt"),
+        ("test_case_2", "non_existent_directory", "file2.png", "file2.png"),
+    ])
+    def test_rename_file_source_and_destination_filenames_are_same(
+            self,
+            scenario_name: str,
+            path: str,
+            source_filename: str,
+            dest_filename: str) -> None:
+        self.subject.rename(path, source_filename, dest_filename)
+        self._validate_no_change_in_file()
 
+    def _validate_no_change_in_file(self) -> None:
+        self.assertTrue(os.path.exists(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME)))
 
+    @parameterized.expand([
+        ("test_case_1", "invalid_directory", "TEST_FILE_NAME", "file1.txt"),
+        # ("test_case_2", "directory", " ", "file2.png"),
+    ])
+    def test_rename_file_source_file_does_not_exist(
+            self,
+            scenario_name: str,
+            path: str,
+            source_filename: str,
+            dest_filename: str) -> None:
+
+        with self.assertRaises(FileNotFoundError) as e:
+            self.subject.rename(path=path, source_filename=source_filename,destination_filename=dest_filename)
+        source_filepath: str = os.path.join(path, source_filename)
+        self.assertEqual(str(e.exception), f"Source file does not exist: '{source_filepath}'")
