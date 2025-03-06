1a492a933190fa5e93e1a1c1d66ec86d1c00fea8
vaishaligpt25 <vaishali.gpt25@gmail.com>
2025-03-06 06:08:27 +0000

    [unit-test] (TestSimpleFileRenameApplier) Add test for destination file already exists

--- tst/rename_applier/test_simple_rename_applier.py
+++ tst/rename_applier/test_simple_rename_applier.py
@@ -1,28 +1,35 @@
 import os
+import shutil
 from parameterized import parameterized
 from unittest import TestCase
-from typing import override
+from typing import override, List
 from src.professional_file_renamer.rename_applier.simple_rename_applier import SimpleRenameApplier
 
 
 class TestSimpleRenameApplier(TestCase):
     TEST_DIR_NAME: str = "test_dir"
-    TEST_FILE_NAME: str = "test_file.txt"
+    TEST_FILE_NAME_1: str = "test_file_1.txt"
+    TEST_FILE_NAME_2: str = "test_file_2.json"
+    TEST_FILE_NAMES: List[str] = [TEST_FILE_NAME_1, TEST_FILE_NAME_2]
 
     subject: SimpleRenameApplier
 
     @override
     def setUp(self):
         self.subject: SimpleRenameApplier = SimpleRenameApplier()
+        self.tearDown()
         # create sample test directory and file
         os.mkdir(self.TEST_DIR_NAME)
-        open(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME), "w").close()
+        for test_file_name in TestSimpleRenameApplier.TEST_FILE_NAMES:
+            open(os.path.join(self.TEST_DIR_NAME, test_file_name), "w").close()
 
     @override
     def tearDown(self):
         # delete sample test directory and file
-        os.remove(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME))
-        os.rmdir(self.TEST_DIR_NAME)
+        # os.remove(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME))
+        # os.rmdir(self.TEST_DIR_NAME)
+        if os.path.exists(self.TEST_DIR_NAME):
+            shutil.rmtree(self.TEST_DIR_NAME)
 
     @parameterized.expand([
         ("test_case_1", "non_existent_directory", "file1.txt", "file1.txt"),
@@ -38,10 +45,11 @@ class TestSimpleRenameApplier(TestCase):
         self._validate_no_change_in_file()
 
     def _validate_no_change_in_file(self) -> None:
-        self.assertTrue(os.path.exists(os.path.join(self.TEST_DIR_NAME, self.TEST_FILE_NAME)))
+        for test_file_name in TestSimpleRenameApplier.TEST_FILE_NAMES:
+            self.assertTrue(os.path.exists(os.path.join(self.TEST_DIR_NAME, test_file_name)))
 
     @parameterized.expand([
-        ("test_case_1", "invalid_directory", TEST_FILE_NAME, "file1.txt"),
+        ("test_case_1", "invalid_directory", TEST_FILE_NAME_1, "file1.txt"),
         ("test_case_2", TEST_DIR_NAME, "file1.puchi", "file2.png"),
     ])
     def test_rename_file_source_file_does_not_exist(
@@ -55,5 +63,24 @@ class TestSimpleRenameApplier(TestCase):
             self.subject.rename(path=path, source_filename=source_filename,destination_filename=dest_filename)
         source_filepath: str = os.path.join(path, source_filename)
         self.assertEqual(str(e.exception), f"Source file does not exist: '{source_filepath}'")
+
+        self._validate_no_change_in_file()
+
+
+    @parameterized.expand([
+        ("test_case_1", TEST_DIR_NAME, TEST_FILE_NAME_2, TEST_FILE_NAME_1),
+        ("test_case_2", TEST_DIR_NAME, TEST_FILE_NAME_1, TEST_FILE_NAME_2),
+    ])
+    def test_rename_file_destination_file_already_exists(
+            self,
+            scenario_name: str,
+            path: str,
+            source_filename: str,
+            dest_filename: str) -> None:
+
+        with self.assertRaises(FileExistsError) as e:
+            self.subject.rename(path=path, source_filename=source_filename, destination_filename=dest_filename)
+        dest_filepath: str = os.path.join(path, dest_filename)
+        self.assertEqual(str(e.exception), f"Destination file already exists: '{dest_filepath}'")
 
         self._validate_no_change_in_file()
